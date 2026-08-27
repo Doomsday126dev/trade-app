d4d03a34ac30eac4c77a35086f409aac50e1a8d2
Ami126 <amitya126@gmail.com>
2026-08-27 09:10:51 -0400

    chore: prepare release 2026-08-27.71

--- js/domain/clientRelease.js
+++ js/domain/clientRelease.js
@@ -1,6 +1,6 @@
 (function(global){
   const root=global.PogoDomain=global.PogoDomain||{};
-  const RELEASE_ID='2026-08-26.70';
+  const RELEASE_ID='2026-08-27.71';
   const TRAINER_DISCOVERY_API=Object.freeze(['fold','trainerSuggestions','bestTrainerSuggestion']);
 
   function trainerSearchControlState(domain){
