984c73dfb25aa12d395855d5a07307d01136445f
Ami126 <amitya126@gmail.com>
2026-08-27 20:47:29 -0400

    chore: prepare release 2026-08-27.73

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 //     trimmed by max entry count to keep storage bounded
 //   - Firebase realtime endpoints: never cached (always network)
 
-const RELEASE='2026-08-27.72';
+const RELEASE='2026-08-27.73';
 const VERSION=`pogo-trades-${RELEASE}`;
 const SHELL_CACHE=`shell-${VERSION}`;
 const INSTALL_CACHE=`${SHELL_CACHE}-installing`;
