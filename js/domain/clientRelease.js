fd0c8d4fc84c94f24a2203a32ad7cea192888fae
Ami126 <amitya126@gmail.com>
2026-08-31 09:30:38 -0400

    chore: prepare privacy notice release 2026-08-31.86

--- js/domain/clientRelease.js
+++ js/domain/clientRelease.js
@@ -1,6 +1,6 @@
 (function(global){
   const root=global.PogoDomain=global.PogoDomain||{};
-  const RELEASE_ID='2026-08-31.85';
+  const RELEASE_ID='2026-08-31.86';
   const TRAINER_DISCOVERY_API=Object.freeze(['fold','trainerSuggestions','bestTrainerSuggestion']);
 
   function trainerSearchControlState(domain){
